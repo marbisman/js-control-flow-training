5388d3ef6472c54486ea807a6a387469889f6f40
marbisman <maria.arbisman@gmail.com>
2017-09-28 09:37:17 -0700

    homework updated with class notes

--- starter-code/login.js
+++ starter-code/login.js
@@ -20,6 +20,16 @@ for (i=0; i < securityQuestions.length; i++){
   }
 };
 
+var passwordInput;
+var loggedIn;
+
+while (!loggedIn){
+  passwordInput = prompt("Enter password for user " + userLogin.userName + ".")
+  if (passwordInput === userLogin.password){
+    console.log ("Welcome!");
+  }
+}
+
 //prompt users for their password
 //var userPassword = prompt("Enter password for user" + userLogin[0].userName)
 
--- starter-code/sing.js
+++ starter-code/sing.js
@@ -2,21 +2,24 @@ var bottles;
 for (i = 5; i >= 1; i--)
 {
     if (i == 1) {
-        bottles = 'bottle';
+        bottles = "bottle";
     } else {
-        bottles = 'bottles';
+        bottles = "bottles";
     }
+
     console.log(i +" "+bottles+" of beer on the wall.");
 
     if (i < 5) {
         console.log("");
         console.log(i+" "+bottles+" of beer on the wall.");
     }
+
+
     console.log(i+" "+bottles+" of beer.");
     console.log("Take one down.");
     console.log("Pass it around.");
 
     if (i == 1) {
-        console.log("No bottles of beer on the wall.");
+        console.log("No more bottles of beer on the wall.");
     }
 }

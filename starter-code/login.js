cd9247230e1cb3c61bb16a6f5c8e7b94abfeb50b
marbisman <maria.arbisman@gmail.com>
2017-09-28 09:00:50 -0700

    Homework completed

--- starter-code/login.js
+++ starter-code/login.js
@@ -1,1 +1,31 @@
 console.log("login.js loaded");
+
+var securityQuestions = [
+  {
+  question:"What is your mother's maiden name?",
+  expectedAnswer:"Smith",
+  }
+];
+
+var userLogin = {
+  userName:"Jan",
+  password:"12345"
+};
+
+//use prompt to create a popup in the browswer with the question
+for (i=0; i < securityQuestions.length; i++){
+  var userAnswer = prompt(securityQuestions[i].question);
+  if (userAnswer != securityQuestions[i].expectedAnswer){
+    alert("sorry, that is incorrect");
+  }
+};
+
+//prompt users for their password
+//var userPassword = prompt("Enter password for user" + userLogin[0].userName)
+
+// numIterations ensures this while loop doesn't go on forever
+//var numIterations = 0;
+//while (true && numIterations < 100){
+  //console.log("still going!");
+  //numIterations++;
+//}
--- starter-code/sing.js
+++ starter-code/sing.js
@@ -1,1 +1,22 @@
-console.log("sing.js loaded");
+var bottles;
+for (i = 5; i >= 1; i--)
+{
+    if (i == 1) {
+        bottles = 'bottle';
+    } else {
+        bottles = 'bottles';
+    }
+    console.log(i +" "+bottles+" of beer on the wall.");
+
+    if (i < 5) {
+        console.log("");
+        console.log(i+" "+bottles+" of beer on the wall.");
+    }
+    console.log(i+" "+bottles+" of beer.");
+    console.log("Take one down.");
+    console.log("Pass it around.");
+
+    if (i == 1) {
+        console.log("No bottles of beer on the wall.");
+    }
+}
